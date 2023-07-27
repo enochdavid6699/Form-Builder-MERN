6f811ac17735354916efb74a7fa989631df12cf2
Enoch <enochdavid6699@gmail.com>
2023-07-27 16:33:01 +0530

    Setting Up Routes and Controller

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use exxpress router
+app.use( '/' , require('./routes/index' ));
+
 
 app.listen(port, function (err) {
     if (err) {
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+module.exports=router;
+
+const homeController = require('../controller/home_controller');
+router.get('/' , homeController.home);
